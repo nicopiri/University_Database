f3c39aaccee972368d91c4ab3d1d4030bc420999
Nico <nicopir@outlook.it>
2023-07-14 17:26:20 +0200

    protected_insert

--- .venv/protected_insert.py
+++ .venv/protected_insert.py
@@ -2,28 +2,32 @@ import psycopg2
 import connection
 import bleach
 
-def insert_utenti(id_utente, nome, cognome, codice_fiscale, luogo_nascita, data_nascita, data_immatricolazione, ruolo):
+
+def insert_utenti(nome, cognome, codice_fiscale, luogo_nascita, data_nascita):
     try:
         conn = connection.open_db()
         cursor = conn.cursor()
-        query = "INSERT INTO utenti (id_utente, nome, cognome, codice_fiscale, luogo_nascita, data_nascita, data_immatricolazione, ruolo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        query = """
+                    INSERT INTO utenti (nome, cognome, codice_fiscale, luogo_nascita, data_nascita, data_immatricolazione, ruolo) 
+                    VALUES (%s, %s, %s, %s, %s, CURRENT_DATE, 1)
+                    RETURNING id_utente
+                """
         values = (
-            bleach.clean(id_utente),
             bleach.clean(nome),
             bleach.clean(cognome),
             bleach.clean(codice_fiscale),
             bleach.clean(luogo_nascita),
-            bleach.clean(data_nascita),
-            bleach.clean(data_immatricolazione),
-            bleach.clean(ruolo)
+            bleach.clean(data_nascita)
         )
         cursor.execute(query, values)
+        inserted_id = cursor.fetchone()[0]
         conn.commit()
         connection.close_db(exception=None)
-        return 'Data inserted successfully!'
+        return f'Data inserted successfully! ID: {inserted_id}'
 
     except psycopg2.Error as e:
-        return 'Error inserting data: {}'.format(e)
+        return f'Error inserting data: {e}'
+
 
 
 def insert_esame(id_esame, nome, descrizione, min_prove, max_prove, docente_responsabile):
