689247a4ad0d3bc8f07886eee9cb6aaafdd1ce65
FatnorJashari <115466019+FatnorJashari@users.noreply.github.com>
2023-08-03 13:44:31 +0200

    fix for all student

--- front-end_nuovo/prove.js
+++ front-end_nuovo/prove.js
@@ -1,34 +1,42 @@
 const urlParams = new URLSearchParams(window.location.search);
 var studentId = urlParams.get('id');
-async function populateProveTable() {
-    try {
-      const response = await fetch(`http://127.0.0.1:5000/get/prove_valide/${studentId}`); // Update with your API URL
-      const data = await response.json();
-  
-      const proveTable = document.getElementById('prove-table');
-  
 
+async function populateProveTable() {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/get/prove_valide/${studentId}`);
+    const data = await response.json();
   
-      // Loop through the data and create table rows dynamically
-      data.forEach(prova => {
-        const row = proveTable.insertRow();
+    const proveTable = document.getElementById('prove-table');
+    const examOccurrences = {}; // To store the occurrences of each exam name
   
-        for (const cellData of prova) {
-          const cell = row.insertCell();
-          if (typeof cellData === 'boolean') {
-            cell.textContent = cellData ? 'Yes' : 'No';
-          } else if (typeof cellData === 'object') {
-            cell.textContent = new Date(cellData).toLocaleDateString();
-          } else {
-            cell.textContent = cellData;
-          }
+    data.forEach(prova => {
+      const examName = prova[0];
+      if (examOccurrences[examName] === undefined) {
+        examOccurrences[examName] = 1;
+      } else {
+        examOccurrences[examName]++;
+      }
+  
+      const row = proveTable.insertRow();
+      const cell = row.insertCell();
+      cell.textContent = `${examName} ${examOccurrences[examName]}`;
+  
+      for (let i = 2; i < prova.length; i++) {
+        const cellData = prova[i];
+        const cell = row.insertCell();
+  
+        if (typeof cellData === 'boolean') {
+          cell.textContent = cellData ? 'Si' : 'No';
+        } else if (typeof cellData === 'object') {
+          cell.textContent = new Date(cellData).toLocaleDateString();
+        } else {
+          cell.textContent = cellData;
         }
-      });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-  
-  // Call the function to populate the table when the page loads
-  populateProveTable();
-  
+}
+
+populateProveTable();
